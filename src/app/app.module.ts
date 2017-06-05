d400e40513e35f3d3deb705f5d72dd13c7fa1c72
victor <anomaly.stalker@gmail.com>
2017-06-05 13:55:36 +0300

    Divided to 3 components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { WeatherComponent } from './weather/weather.component';
+import { FollowersComponent } from './followers/followers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    WeatherComponent,
+    FollowersComponent
   ],
   imports: [
     BrowserModule
